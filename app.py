d3d2d081098c4c82b4ba5f8c12d9e36cfa8af905
JoaoVitor <joaokussler@gmail.com>
2020-09-08 21:34:21 -0300

    mudancas na conexao com banco

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
 from flask import Flask, session, url_for, redirect, render_template, request, abort, flash
 app = Flask(__name__)
 
 @app.route("/")
 def page_index():
-    return render_template("index.html")
+    return render_template("index.html", html="")
 
-@app.route("/fretes")
-def page_fretes():
-    return 'PAREI AQUI'
+@app.route("/f1live")
+def page_f1live():
+    url = "https://f1.tfeed.net/live"
+    html_content = requests. get(url).text
+    return render_template("index.html", html=html_content)
